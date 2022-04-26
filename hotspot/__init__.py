bd4f342e3006cb1aa1d411b3d8c1726ef80215ab
adamgayoso <adamgayoso@users.noreply.github.com>
2022-04-26 07:33:20 -0700

    poetry build

--- hotspot/__init__.py
+++ hotspot/__init__.py
@@ -1,4 +1,12 @@
-from ._version import __version__
 name = "hotspot"
 
 from .hotspot import Hotspot
+
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+# https://github.com/python-poetry/poetry/issues/144#issuecomment-623927302
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+package_name = "hotspotsc"
+__version__ = importlib_metadata.version(package_name)
--- setup.py
+++ setup.py
@@ -1,38 +1,8 @@
-from setuptools import setup, find_packages
-import os
+#!/usr/bin/env python
 
-# Parse version string
-this_directory = os.path.dirname(os.path.abspath(__file__))
-version_file = os.path.join(this_directory, "hotspot", "_version.py")
-exec(open(version_file).read())
+# This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
+import setuptools
 
-setup(
-    name="hotspot",
-    version=__version__,
-    packages=find_packages(),
-
-    install_requires=[
-        'matplotlib>=3.0.0',
-        'numba>=0.43.1',
-        'numpy>=1.16.4',
-        'seaborn>=0.9.0',
-        'scipy>=1.2.1',
-        'pandas>=0.24.0',
-        'tqdm>=4.32.2',
-        'statsmodels>=0.9.0',
-        'scikit-learn>=0.21.2',
-    ],
-    extras_require=dict(
-        test=['pytest>=5.0.0'],
-    ),
-
-    include_package_data=True,
-
-    author="David DeTomaso",
-    author_email="David.DeTomaso@berkeley.edu",
-    description="",
-    keywords="",
-    url="",
-    license=""
-)
+if __name__ == "__main__":
+    setuptools.setup(name="hotspotsc")
